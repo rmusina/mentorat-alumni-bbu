28dbef9ebc47e1f976a3b28fb9306b3e908ddd71
Vasile Tofan <tephe@users.noreply.github.com>
2011-03-17 22:10:19 +0200

    Added django admin visualization of Mentoring agreements. Linked agreements from profile. Done.

--- src/mentorat/apps/friends/admin.py
+++ src/mentorat/apps/friends/admin.py
@@ -7,8 +7,14 @@ from friends.models import Contact, Friendship, JoinInvitation, \
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'email', 'user', 'added')
 
+class FriendshipMentoringAgreementInline(admin.TabularInline):
+    model = MentoringAgreement
+
 class FriendshipAdmin(admin.ModelAdmin):
     list_display = ('id', 'from_user', 'to_user', 'added',)
+    inlines = [
+        FriendshipMentoringAgreementInline,
+    ]
 
 class JoinInvitationAdmin(admin.ModelAdmin):
     list_display = ('id', 'from_user', 'contact', 'status')
@@ -19,8 +25,33 @@ class FriendshipInvitationAdmin(admin.ModelAdmin):
 class FriendshipInvitationHistoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'from_user', 'to_user', 'sent', 'status',)
 
+class MentoringAgreementObjectivesInline(admin.TabularInline):
+    model = MentoringAgreementObjectives
+
+class MentoringAgreementCommunicationMethodsInline(admin.TabularInline):
+    model = MentoringAgreementCommunicationMethods
+
+class MentoringAgreementProblemsInline(admin.TabularInline):
+    model = MentoringAgreementProblems
+
+class MentoringAgreementActivitesInline(admin.TabularInline):
+    model = MentoringAgreementActivites
+
+class MentoringAgreementObjectiveGoalsInline(admin.TabularInline):
+    model = MentoringAgreementObjectiveGoals
+
+class MentoringAgreementAdmin(admin.ModelAdmin):
+    inlines = [
+        MentoringAgreementObjectivesInline,
+        MentoringAgreementCommunicationMethodsInline,
+        MentoringAgreementActivitesInline,
+        MentoringAgreementObjectiveGoalsInline,
+        MentoringAgreementProblemsInline,
+    ]
+
+
 admin.site.register(Contact, ContactAdmin)
-admin.site.register(MentoringAgreement)
+admin.site.register(MentoringAgreement, MentoringAgreementAdmin)
 admin.site.register(Friendship, FriendshipAdmin)
 admin.site.register(JoinInvitation, JoinInvitationAdmin)
 admin.site.register(FriendshipInvitation, FriendshipInvitationAdmin)
