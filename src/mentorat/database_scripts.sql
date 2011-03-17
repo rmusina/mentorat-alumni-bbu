c0423aaf251181061606e340c5d8e2b9e5510049
Vasile Tofan <tephe@users.noreply.github.com>
2011-03-17 18:55:16 +0200

    Added alter script.

--- src/mentorat/database_scripts.sql
+++ src/mentorat/database_scripts.sql
@@ -1,3 +1,6 @@
 /* ADD mentor visibility to profile */
 ALTER TABLE profiles_mentorprofile ADD visible_to_mentors BIT NOT NULL DEFAULT(0);
 ALTER TABLE profiles_studentprofile DROP COLUMN mentor_expectations;
+/* ADDS foreign key relation to MentoringAgreement in Friendship */
+ALTER TABLE friends_friendship ADD COLUMN mentoring_agreement_id INT UNIQUE;
+ALTER TABLE friends_friendship ADD CONSTRAINT fk_1 FOREIGN KEY (mentoring_agreement_id) REFERENCES friends_mentoringagreement (id); 
